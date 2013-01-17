d392761820e0252a1251b113bd560a2d968d4ffe
Thibaut Marmin <marminthibaut@gmail.com>
2013-01-17 15:16:47 +0100

    yahou

--- src/com/github/masalthunlass/complexgen/InitTDB.java
+++ src/com/github/masalthunlass/complexgen/InitTDB.java
@@ -2,6 +2,8 @@ package com.github.masalthunlass.complexgen;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 
 import com.github.masalthunlass.complex.model.enums.DataEnum;
 
@@ -13,22 +15,25 @@ public class InitTDB {
 	private static HashMap<String, String> file_path;
 
 	private static void initPathFiles() {
-//		file_path.put("inseepop", "/resources/data/rdf/inseepop.rdf");
-//		file_path.put("inseecog", "/resources/data/rdf/inseecog.rdf");
-//		file_path.put("geonames", "/resources/data/rdf/geonames.rdf");
+		// file_path.put("inseepop", "/resources/data/rdf/inseepop.rdf");
+		// file_path.put("inseecog", "/resources/data/rdf/inseecog.rdf");
+		// file_path.put("geonames", "/resources/data/rdf/geonames.rdf");
 		file_path.put("passim", "/resources/data/rdf/passim.rdf");
-//		file_path.put("isf", "/resources/data/rdf/isf.rdf");
-//		file_path.put("monuments", "/resources/data/rdf/monuments.rdf");
+		// file_path.put("isf", "/resources/data/rdf/isf.rdf");
+		// file_path.put("monuments", "/resources/data/rdf/monuments.rdf");
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		initPathFiles();
 		System.out.println("Suppression des répertoires TDB");
 		suppressDirectories();
 		System.out.println("Création des répertoires TDB");
 		createDirectories();
+		System.out.println("Importation des données");
+		importData();
 	}
 
 	private static void suppressDirectories() {
@@ -52,4 +57,11 @@ public class InitTDB {
 		}
 	}
 
+	private static void importData() {
+		Set<String> keys = file_path.keySet();
+		for (String key : keys) {
+			String subdirname = key;
+			String rdfpath = file_path.get(key);
+		}
+	}
 }

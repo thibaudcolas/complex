5915d555efc81ba7479aa2b8775b90252f6a3c0b
Thibaud Colas <thibaudcolas@gmail.com>
2013-01-17 04:00:25 +0100

    Add all Web main files + first draft of the interface is now in the JSP

---
+++ WebContent/js/main.js
@@ -0,0 +1,103 @@
+jQuery(document).ready(function($) {
+
+  /**
+   * Query
+   * ---------------------------------------------------------------------
+   */
+
+  var $querySelect = $('#query-select');
+  var $queryNamespaces = $('.query-namespaces');
+
+  // Creates the CodeMirror editor.
+  var queryEditor = CodeMirror.fromTextArea(document.getElementById("query-editor"), {
+    mode: "application/x-sparql-query",
+    tabMode: "indent",
+    matchBrackets: true,
+    lineNumbers: true,
+    lineWrapping: true,
+    theme: "elegant"
+  });
+
+  // Plugin to match selected tokens.
+  queryEditor.on("cursorActivity", function() {
+    queryEditor.matchHighlight("CodeMirror-matchhighlight");
+  });
+
+  // At each query change, we must reload the editor with the new query and reload the namespaces.
+  $querySelect.change(function(e){
+    selected = $(this).find(':selected');
+    queryEditor.setValue(selected.attr('data-query'));
+    reloadReferencedNamespaces();
+  });
+
+  // Loads the queries from a JSON file.
+  $.getJSON('data/queries.json', function(data) {
+    var firstQuery;
+    $.each(data, function(key, query) {
+      // Initialize the editor with the first Query.
+      firstQuery = !firstQuery ? query : firstQuery;
+      // All the queries are put into the select with custom data attributes.
+      $querySelect.append('<option value="'+key+'" data-query="'+query.string+'" data-description="'+query.description+'">'+query.title+'</option>');
+    });
+    $querySelect.first().attr('selected', true);
+    queryEditor.setValue($('<div/>').html(firstQuery.string).text());
+    reloadReferencedNamespaces();
+  });
+
+  // Manages extracting namespace URIs from the query string.
+  function extractNamespaces(queryString) {
+    queryString = queryString.split('SELECT')[0];
+    namespaces = queryString.match(/ (.*?): <(.*?)>/mg);
+    return namespaces;
+  }
+
+  // Transforms namespace URIs into links pointing to documentations.
+  function displayNamespacesURLs(queryString) {
+    namespaces = extractNamespaces(queryString);
+    $queryNamespaces.empty();
+    for (var i = 0; i < namespaces.length; i++) {
+      prefix = namespaces[i].replace(/>/,'').split(/: </);
+      $queryNamespaces.append('<li>'+prefix[0]+': <a href="'+prefix[1]+'">'+prefix[1]+'</a></li>');
+    }
+  }
+
+  function reloadReferencedNamespaces() {
+    displayNamespacesURLs(queryEditor.getValue());
+  }
+
+  function prependToEditor(str) {
+    queryEditor.setValue(str + queryEditor.getValue());
+  }
+
+  // Calls the prefix.cc API to add namespaces to our query.
+  $('#prefix-button').click(function (e){
+    e.preventDefault();
+    prefixId = $('#prefix-text').val();
+    $.getJSON('http://prefix.cc/'+prefixId+'.file.json', function(data) {
+      newPrefix = 'PREFIX ' + prefixId + ': ';
+      $.each(data, function(key, val) {
+        prependToEditor('PREFIX ' + prefixId + ': ' + '<' + val + '>\n');
+      });
+      reloadReferencedNamespaces();
+    });
+  });
+
+  /**
+   * Results
+   * ---------------------------------------------------------------------
+   */
+
+  $('.observation-table').dataTable({
+    'aoColumns': [
+      {
+        'sType': 'html'
+      },
+      {
+        'sType': 'data-numeric'
+      }
+    ]
+  });
+
+});
+
+
